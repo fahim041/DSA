da2d151795a8f59557a79c6ca56ab7c2fcf1bc8a
Fahim090 <FAlSami@grantmatch.com>
2023-10-01 14:43:31 -0400

    70_climbing_stairs

---
+++ src/leetcode/dp/l_70_climbing_stairs.java
@@ -0,0 +1,16 @@
+package leetcode.dp;
+
+public class l_70_climbing_stairs {
+    public int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+
+        for(int i = 0; i < n - 1; i++){
+            int tmp = one;
+            one = one + two;
+            two = tmp;
+        }
+
+        return one;
+    }
+}
