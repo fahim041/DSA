e11032ff0b13673f04b057230afea6d4fc174f0f
Fahim090 <FAlSami@grantmatch.com>
2023-10-01 22:30:15 -0400

    reset main

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,12 +4,5 @@ import datastructure.*;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
-        doublyLinkedList.addAtTail(5);
-        doublyLinkedList.addAtTail(3);
-        doublyLinkedList.addAtIndex(1, 99);
-        doublyLinkedList.addAtIndex(3, 55);
-        doublyLinkedList.deleteAtIndex(1);
-        doublyLinkedList.print();
     }
 }
