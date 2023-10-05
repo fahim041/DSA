81e93509417147a99246c2c3a8694d55797ea8e9
Fahim090 <FAlSami@grantmatch.com>
2023-10-05 14:29:42 -0400

    insertion sort

---
+++ src/algorithms/InsertionSort.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class InsertionSort {
+    public int[] sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            while (j >= 0 && arr[j] > arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                j--;
+            }
+        }
+
+        return arr;
+    }
+}
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,5 @@
 package com.main;
 
-import datastructure.*;
-
 public class Main {
     public static void main(String[] args) {
     }
