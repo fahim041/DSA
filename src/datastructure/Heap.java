592cbf4d4b88861cfc48039790c200fde71831ad
Fahim090 <FAlSami@grantmatch.com>
2023-10-09 13:57:25 -0400

    heapify

--- src/datastructure/Heap.java
+++ src/datastructure/Heap.java
@@ -56,4 +56,38 @@ public class Heap {
 
         return res;
     }
+
+    public void heapify(ArrayList<Integer> arr) {
+        // add the first in the last index
+        arr.add(arr.get(0));
+
+        // assign the arr to heap
+        heap = new ArrayList<>(arr);
+
+        // get the index of the value which has child
+        int curr = (heap.size() - 1) / 2;
+
+        while (curr > 0) {
+            int i = curr;
+            while (2 * i < heap.size()) {
+                if (2 * i + 1 < heap.size() &&
+                        heap.get(2 * i + 1) < heap.get(2 * i) &&
+                        heap.get(i) > heap.get(2 * i + 1)) {
+                    int tmp = heap.get(i);
+                    heap.set(i, heap.get(2 * i + 1));
+                    heap.set(2 * i + 1, tmp);
+                    i = 2 * i + 1;
+                } else if (heap.get(i) > heap.get(2 * i)) {
+                    int tmp = heap.get(i);
+                    heap.set(i, heap.get(2 * i));
+                    heap.set(2 * i, tmp);
+                    i = 2 * i;
+                } else {
+                    break;
+                }
+            }
+            curr--;
+        }
+
+    }
 }
