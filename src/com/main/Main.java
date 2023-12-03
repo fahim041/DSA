e2385c813954518d648901efca639e08c0266f6a
Fahim090 <FAlSami@grantmatch.com>
2023-12-02 22:26:00 -0500

    sync

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,6 +1,14 @@
 package com.main;
 
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.add(-4);
+        heap.add(-6);
+        heap.add(-11);
+        heap.add(-9);
+        System.out.println(heap);
     }
 }
